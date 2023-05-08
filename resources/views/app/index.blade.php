bd65aaeb4804e70e5285d8a6aa58140ca638c16d
tinemanabs <christinemmanabat@gmail.com>
2023-05-08 19:55:43 +0800

    static pages // index page

---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,361 @@
+@extends('layouts.app')
+
+@section('content')
+    {{-- COTTAGES --}}
+    <div id="cottages">
+        <div class="container section-content">
+            <h3>Our Cottages</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p>
+
+            <div class="lightbox-gallery">
+                <div class="container">
+                    <div class="row photos">
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/1.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/1.png') }}">
+                            </a>
+                        </div>
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/2.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/2.png') }}">
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/3.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/3.png') }}">
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/4.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/4.png') }}">
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/5.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/5.png') }}">
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/6.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/6.png') }}">
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/7.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/7.png') }}">
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <a href="{{ asset('img/cottages/8.png') }}" data-lightbox="photos">
+                                <img class="img-fluid" src="{{ asset('img/cottages/8.png') }}">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    {{-- END OF COTTAGES SECTION --}}
+
+    {{-- ROOMS --}}
+    <div id="rooms">
+        <div class="container section-content">
+            <h3>Our Rooms</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p>
+            <div class="lightbox-gallery">
+                <div class="container">
+                    <div class="row rooms d-flex justify-content-center">
+                        <div class="col-12 col-md-6 item">
+                            <img class="img-fluid" src="{{ asset('img/rooms/1.png') }}">
+                        </div>
+                        <div class="col-12 col-md-6 item">
+                            <img class="img-fluid" src="{{ asset('img/rooms/2.png') }}">
+                        </div>
+
+                        <div class="col-12 col-md-6 item">
+                            <img class="img-fluid" src="{{ asset('img/rooms/3.png') }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- END OF ROOMS SECTION --}}
+
+    {{-- AMMENITIES --}}
+    <section id="ammenities">
+        <div class="container section-content">
+            <h3>Our Ammenities</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p>
+
+            <!-- Swiper -->
+            <div class="swiper ammenitiesSwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/1.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Inclinator</h5>
+                                <p>We offer inclinator to our guests who are staying in “BABA” since it will take
+                                    75 steps before you reach the ground.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/2.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Sari-sari Store</h5>
+                                <p>We have a sari-sari store that sell snacks, drinks,
+                                    swimming essentials, frozen goods, and toiletries. It also offers rentable items such as
+                                    karaoke and floaters.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/3.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Parking Space</h5>
+                                <p>We offer limited parking space to our guests. We encourage our guests to let us know if
+                                    they they will be needing a parking slot.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/4a.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Live Animals</h5>
+                                <p>Our resort is known as “nature resort” which we offer our guests an ambience close to
+                                    nature for a perfect getaway.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/4b.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Live Animals</h5>
+                                <p>Our resort is known as “nature resort” which we offer our guests an ambience close to
+                                    nature for a perfect getaway.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/5a.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Animal Sculptures</h5>
+                                <p>Our resort also have some animal sculptures. It is perfect for
+                                    the kids and family.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card ammenities-card">
+                            <img class="card-img-top" src="{{ asset('img/ammenities/5b.png') }}" alt="">
+                            <div class="card-body">
+                                <h5>Animal Sculptures</h5>
+                                <p>Our resort also have some animal sculptures. It is perfect for
+                                    the kids and family.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </section>
+
+    {{-- END OF AMMENITIES SECTION --}}
+
+    {{-- POOLS --}}
+    <div id="pools">
+        <div class="container section-content">
+            <h3>Our Pools</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p>
+
+            <div class="lightbox-gallery">
+                <div class="container">
+                    <div class="row pools d-flex justify-content-center">
+                        <div class="col-12 col-md-4 item">
+                            <img class="img-fluid" src="{{ asset('img/pools/1.png') }}">
+                        </div>
+                        <div class="col-12 col-md-4 item">
+                            <img class="img-fluid" src="{{ asset('img/pools/2.png') }}">
+                        </div>
+
+                        <div class="col-12 col-md-4 item">
+                            <img class="img-fluid" src="{{ asset('img/pools/3.png') }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- END OF POOLS SECTION --}}
+
+    {{-- ABOUT US --}}
+    <div id="aboutus">
+        <div class="container section-content">
+            <h3>About Us</h3>
+
+            <div class="row d-flex align-items-center mb-3">
+                <div class="col-12 col-md-6 order-md-last">
+                    <img src="{{ asset('img/aboutus/1.png') }}" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 ">
+                    <h1>Labak sa Morong</h1>
+                    <p>is a nature resort in Sariaya, Quezon that offers a perfect getaway to its local and tourists. </p>
+                </div>
+            </div>
+
+            <div class="row d-flex align-items-center">
+                <div class="col-12 col-md-6 order-md-first">
+                    <img src="{{ asset('img/aboutus/2.png') }}" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 ">
+                    <h1>Locate Us</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- END OF ABOUT US SECTION --}}
+
+    {{-- EVENTS --}}
+    <div id="events">
+        <div class="container section-content">
+            {{-- <h3>Events</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p> --}}
+
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-6 order-md-last">
+                    <img src="{{ asset('img/events.png') }}" alt="" class="img-fluid rounded-3">
+                </div>
+                <div class="col-12 col-md-6 text-center">
+                    <h5>Celebrate your milestone with us!</h5>
+                    <button class="btn btn-primary">Book Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- END OF EVENTS SECTION --}}
+
+    {{-- DEALS --}}
+    <div id="deals">
+        <div class="container section-content">
+            <h3>Deals</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p>
+
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-6 mb-3">
+                    <img src="{{ asset('img/deals/1.png') }}" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6">
+                    <img src="{{ asset('img/deals/2.png') }}" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- END OF DEALS SECTION --}}
+
+    {{-- REVIEWS --}}
+    <div id="reviews">
+        <div class="container section-content">
+            <h3>Reviews</h3>
+            <p class="subheading">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis assumenda
+                doloribus! Facilis ad
+                error, deleniti quidem recusandae architecto eos consequatur placeat aut</p>
+
+            <!-- Swiper -->
+            <div class="swiper reviewsSwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="card reviews-card">
+                            <div class="card-body d-flex justify-content-center align-items-center flex-column">
+                                <i class="fa-solid fa-quote-left fa-5x mb-3"></i>
+                                <i class="fa-regular fa-circle-user fa-4x"></i>
+                                <h5>Juan Miguel Sebastian</h5>
+                                <div class="d-flex align-items-center py-1">
+                                    <span>5/5</span>
+                                    <i class="fa-solid fa-star ms-2" style="color:#ffc107"></i>
+                                </div>
+                                <p>Superb facilities and amenities!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card reviews-card">
+                            <div class="card-body d-flex justify-content-center align-items-center flex-column">
+                                <i class="fa-solid fa-quote-left fa-5x mb-3"></i>
+                                <i class="fa-regular fa-circle-user fa-4x"></i>
+                                <h5>Michael King</h5>
+                                <div class="d-flex align-items-center py-1">
+                                    <span>4/5</span>
+                                    <i class="fa-solid fa-star ms-2" style="color:#ffc107"></i>
+                                </div>
+                                <p>Super helpful ng purchase service nila, hindi
+                                    na ako aakyat pa. Thank you sa mga person in charge
+                                    sa store. Napaka-accommodating.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="card reviews-card">
+                            <div class="card-body d-flex justify-content-center align-items-center flex-column">
+                                <i class="fa-solid fa-quote-left fa-5x mb-3"></i>
+                                <i class="fa-regular fa-circle-user fa-4x"></i>
+                                <h5>James Sagad</h5>
+                                <div class="d-flex align-items-center py-1">
+                                    <span>4/5</span>
+                                    <i class="fa-solid fa-star ms-2" style="color:#ffc107"></i>
+                                </div>
+                                <p>Highly recommended sa mga naghahanap ng resort
+                                    sa Quezon.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+                {{-- <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div> --}}
+
+            </div>
+        </div>
+    </div>
+    {{-- END OF REVIEWS SECTION --}}
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,24 +10,42 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <!-- Swiper CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
+    <!-- Lightbox CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -53,13 +72,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -73,11 +93,108 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
+        <header id="header">
+            <div class="banner-img">
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+                <div class="container-fluid">
+                    <a class="navbar-brand d-block d-lg-none" href="#">
+                        <img src="{{ asset('img/logo.png') }}" alt="" width="100" height="100">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center " id="navbarNavAltMarkup">
+                        <div class="navbar-nav align-items-lg-center">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#cottages">Cottages</a>
+                            <a class="nav-link" href="#rooms">Rooms</a>
+                            <a class="nav-link" href="#ammenities">Ammenities</a>
+                            <a class="nav-link" href="#pools">Pools</a>
+                            <a class="navbar-brand d-none d-lg-block" href="#">
+                                <img src="{{ asset('img/logo.png') }}" alt="" width="100" height="100">
+                            </a>
+                            <a class="nav-link" href="#aboutus">About Us</a>
+                            <a class="nav-link" href="#events">Events</a>
+                            <a class="nav-link" href="#deals">Deals</a>
+                            <a class="nav-link" href="#reviews">Reviews</a>
+                            <a class="nav-link" href="#">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
-        <main class="py-4">
+            <div class="container h-100">
+                <div class="h-100 d-flex justify-content-center align-items-center flex-column banner-content">
+                    <h1>Perfect Getaway</h1>
+                    <p>away from metro</p>
+                    <button class="btn btn-light">Book Now</button>
+                </div>
+            </div>
+        </header>
+
+        <main>
             @yield('content')
         </main>
     </div>
+
+    {{-- Bootstrap JS Bundle --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    {{-- Lightbox JS --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".ammenitiesSwiper", {
+            slidesPerView: 1,
+            spaceBetween: 20,
+
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                }
+            },
+
+            keyboard: {
+                enabled: true,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+
+        var swiper = new Swiper(".reviewsSwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+
+            keyboard: {
+                enabled: true,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            // navigation: {
+            //     nextEl: ".swiper-button-next",
+            //     prevEl: ".swiper-button-prev",
+            // },
+        });
+    </script>
 </body>
+
 </html>
