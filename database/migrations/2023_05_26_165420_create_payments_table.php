35c95cab92535a634dca3afffb44ccf55535f191
robbychristian <robbychristiandeleon@gmail.com>
2023-05-28 19:46:19 +0800

    Update 2023_05_26_165420_create_payments_table.php

--- database/migrations/2023_05_26_165420_create_payments_table.php
+++ database/migrations/2023_05_26_165420_create_payments_table.php
@@ -16,6 +16,7 @@ class CreatePaymentsTable extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('booking_id');
             $table->string('total_paid');
             $table->string('total_price');
             $table->boolean('payment_status');
