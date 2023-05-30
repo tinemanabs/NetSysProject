3d555a54e6a986e959f042539867c1a29987a58f
tinemanabs <christinemmanabat@gmail.com>
2023-05-31 01:38:55 +0800

    Update booknow.blade.php

--- resources/views/features/booknow.blade.php
+++ resources/views/features/booknow.blade.php
@@ -6,12 +6,4 @@
     @elseif (Auth::user()->user_role == 2)
         @include('features.booking.guest')
     @endif
-
-    {{-- NOTE:
-        - POPULATE THE DATES THAT ARE ALREADY RESERVED 
-        - DATE SHOULD BE DRAGGABLE INTO TO 2 DATES
-        - DISABLE THE DAY / NIGHT IF NOT AVAILABLE
-        - FILL OUT THE GUEST INFO FORMS
-        - DESIGN THE UI FOR SUMMARY
-        --}}
 @endsection
